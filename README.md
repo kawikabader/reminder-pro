cdc088bfc2dfd2ae6765b1f783ee6bb63240db32
Kawika Bader <ekbader@gmail.com>
2017-09-10 11:36:24 -0700

    Updated readme. Created redux store, added provider to index. Created reducer and passed the reducer to the store.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Reminder Pro
 
 This application allows users to create and display a list of reminders with custom due dates, and stores all of this data in browser cookies, so as to persist the data on refresh.
+
+Built with:
+- React
+- Redux
+- Bootstrap
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.jsx";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from './reducers';
+
+// Create the initial store
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <App />, document.getElementById('root')
-)
+  // Pass store into redux provider
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);

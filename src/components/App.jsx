3eb4a2c64759c47a71d233eab890b60e19186864
Kawika Bader <ekbader@gmail.com>
2017-09-11 19:47:33 -0700

    Added due date property to add reminder action. Added due date to state and passed it to action. Returning due date in the reminder reducer.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,13 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: ""
+      text: "",
+      dueDate: ''
     };
   }
 
   addReminder() {
-    this.props.addReminder(this.state.text);
+    this.props.addReminder(this.state.text, this.state.dueDate);
   }
 
   deleteReminder(id) {
@@ -51,6 +52,11 @@ class App extends Component {
               className="form-control"
               placeholder="I have to..."
               onChange={event => this.setState({ text: event.target.value })}
+            />
+            <input
+              className="form-control"
+              type="datetime-local"
+              onChange={event => this.setState({dueDate: event.target.value})}
             />
           </div>
           <button
